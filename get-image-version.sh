8015dee45abbf3f7de55adfa6258b02cf99949c1
kaldev-a <kaled.alazem@gmail.com>
2025-07-04 11:00:38 +0100

    simplified my approach to automation

--- get-image-version.sh
+++ get-image-version.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# get-version.sh
-# IMAGE_VERSION=$(node -p "require('./package.json').version")
-APP_IMAGE="app:${IMAGE_VERSION}"
-WEB_IMAGE="web:${IMAGE_VERSION}"
-PROJECT_NAME="project_${IMAGE_VERSION}"
-
-# Update or insert IMAGE_APP_NAME
-grep -q '^IMAGE_APP_NAME=' .env && \
-  sed -i "s/^IMAGE_APP_NAME=.*/IMAGE_APP_NAME=${APP_IMAGE}/" .env || \
-  echo "IMAGE_APP_NAME=${APP_IMAGE}" >> .env
-
-# Update or insert IMAGE_WEB_NAME
-grep -q '^IMAGE_WEB_NAME=' .env && \
-  sed -i "s/^IMAGE_WEB_NAME=.*/IMAGE_WEB_NAME=${WEB_IMAGE}/" .env || \
-  echo "IMAGE_WEB_NAME=${WEB_IMAGE}" >> .env
-
-grep -q '^PROJECT_NAME=' .env && \
-  sed -i "s/^PROJECT_NAME=.*/PROJECT_NAME=${PROJECT_NAME}/" .env || \
-  echo "PROJECT_NAME=${PROJECT_NAME}" >> .env
-
+VERSION=$(jq -r .version package.json)
+echo "::set-output name=version::$VERSION"
+echo ${VERSION}
